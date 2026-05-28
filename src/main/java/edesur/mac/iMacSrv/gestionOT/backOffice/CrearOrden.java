6b5864cbbd45bd3d3866047c107b3b6b2681ed91
ldvalle <34348282+ldvalle@users.noreply.github.com>
2026-05-28 17:14:45 -0300

    Actualizacion

--- src/main/java/edesur/mac/iMacSrv/gestionOT/backOffice/CrearOrden.java
+++ src/main/java/edesur/mac/iMacSrv/gestionOT/backOffice/CrearOrden.java
@@ -1,6 +1,8 @@
 package edesur.mac.iMacSrv.gestionOT.backOffice;
 
 import edesur.mac.iMacSrv.xnear.model.internal.MsgCreacion;
+
+import edesur.mac.iMacSrv.gestionOT.utils.StringTools;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,13 +24,48 @@ public class CrearOrden {
         StringBuilder sb = new StringBuilder(SEL_NUMAO);
         List<Object> params = new ArrayList<>();
         String sNroOrden="";
+        String sNvoNroOrden="";
         long iNroOrden;
+
+        StringTools uStr = new StringTools();
 
         params.add(tipo.trim().toUpperCase());
         params.add(area.trim().toUpperCase());
 
         Optional<String> sValor = jdbcClient.sql(sb.toString()).params(params).query(String.class).optional();
 
+        if(sValor.isEmpty()){
+            sNroOrden = area.trim() + "000001";
+            sNvoNroOrden = area.trim() + "000002";
+
+            params.add(area);
+            params.add(tipo);
+            params.add(sNvoNroOrden);
+            try {
+                jdbcClient.sql(INS_NUMAO).update();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                sNroOrden="X";
+            }
+
+        }else{
+            String sNro= sValor.toString().substring(5);
+            iNroOrden = Long.parseLong(sNro);
+            sNroOrden = sNro;
+            iNroOrden++;
+            sNvoNroOrden = area + uStr.padLeftZeros( Long.toString(iNroOrden),6);
+
+            params.add(sNvoNroOrden);
+            params.add(area);
+            params.add(tipo);
+            try {
+                jdbcClient.sql(UPD_NUMAO).update();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                sNroOrden="X";
+            }
+
+        }
 
 
 
--- src/main/java/edesur/mac/iMacSrv/gestionOT/utils/StringTools.java
+++ src/main/java/edesur/mac/iMacSrv/gestionOT/utils/StringTools.java
@@ -1,24 +1,37 @@
 package edesur.mac.iMacSrv.gestionOT.utils;
 
 public class StringTools {
-    public String TrimStr(String sCadena){
-        return (sCadena!=null) ? sCadena.trim() : null;
+    public String TrimStr(String sCadena) {
+        return (sCadena != null) ? sCadena.trim() : null;
     }
 
-    public String getCampo(String sCadena, int iPos, String sC){
-        String sCampo="";
+    public String getCampo(String sCadena, int iPos, String sC) {
+        String sCampo = "";
 
-        if(sCadena.isEmpty() || sCadena.trim()==""){
+        if (sCadena.isEmpty() || sCadena.trim() == "") {
             return sCampo;
         }
         String[] vecCadena = sCadena.split(sC);
 
-        if(vecCadena.length < iPos){
+        if (vecCadena.length < iPos) {
             return sCampo;
         }
 
-        sCampo=vecCadena[iPos];
+        sCampo = vecCadena[iPos];
 
         return sCampo;
+    }
+
+    public String padLeftZeros(String inputString, int length) {
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - inputString.length()) {
+            sb.append('0');
+        }
+        sb.append(inputString);
+
+        return sb.toString();
     }
 }
